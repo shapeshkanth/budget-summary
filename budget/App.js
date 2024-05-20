2fc9c87d06050c6c3b61a47bc148ad402117ecfc
shapeshkanth <shapeshkanth@gmail.com>
2024-05-20 10:12:33 +0530

    nodejs server created

--- budget/App.js
+++ budget/App.js
@@ -3,13 +3,15 @@ import { NavigationContainer } from '@react-navigation/native';
 import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
 import { Avatar } from 'react-native-elements';
-import  income from './src/income';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'; // Import MaterialCommunityIcons
+import income from './src/income';
 import expense from './src/expense'; 
 import graph from './src/graph'; 
 import summary from './src/summary';
 
 const Drawer = createDrawerNavigator();
 
+// Custom Drawer Content
 const CustomDrawerContent = (props) => {
   return (
     <DrawerContentScrollView {...props}>
@@ -27,6 +29,31 @@ const CustomDrawerContent = (props) => {
   );
 };
 
+// Function to render drawer icons
+const getDrawerIcon = (route, focused, color, size) => {
+  let iconName;
+  switch (route.name) {
+    case 'Home':
+      iconName = focused ? 'home' : 'home-outline';
+      break;
+    case 'Income':
+      iconName = focused ? 'cash' : 'cash-multiple';
+      break;
+    case 'Expenses':
+      iconName = focused ? 'cart' : 'cart-outline';
+      break;
+    case 'Graph':
+      iconName = focused ? 'chart-bar' : 'chart-bar';
+      break;
+    case 'Summary':
+      iconName = focused ? 'format-list-bulleted' : 'format-list-bulleted-type';
+      break;
+    default:
+      iconName = focused ? 'information' : 'information-outline';
+  }
+  return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+};
+
 const App = () => {
   return (
     <NavigationContainer>
@@ -34,11 +61,41 @@ const App = () => {
         initialRouteName="Home"
         drawerContent={props => <CustomDrawerContent {...props} />}
       >
-        <Drawer.Screen name="Home" component={Home} />
-        <Drawer.Screen name="Income">{income}</Drawer.Screen>
-        <Drawer.Screen name="Expenses">{expense}</Drawer.Screen>
-        <Drawer.Screen name="Graph">{graph}</Drawer.Screen>
-        <Drawer.Screen name="Summary">{summary}</Drawer.Screen>
+        <Drawer.Screen 
+          name="Home" 
+          component={Home} 
+          options={({ focused, color, size }) => ({
+            drawerIcon: ({ color, size }) => getDrawerIcon('Home', focused, color, size),
+          })}
+        />
+        <Drawer.Screen 
+          name="Income" 
+          component={income} 
+          options={({ focused, color, size }) => ({
+            drawerIcon: ({ color, size }) => getDrawerIcon('Income', focused, color, size),
+          })}
+        />
+        <Drawer.Screen 
+          name="Expenses" 
+          component={expense} 
+          options={({ focused, color, size }) => ({
+            drawerIcon: ({ color, size }) => getDrawerIcon('Expenses', focused, color, size),
+          })}
+        />
+        <Drawer.Screen 
+          name="Graph" 
+          component={graph} 
+          options={({ focused, color, size }) => ({
+            drawerIcon: ({ color, size }) => getDrawerIcon('Graph', focused, color, size),
+          })}
+        />
+        <Drawer.Screen 
+          name="Summary" 
+          component={summary} 
+          options={({ focused, color, size }) => ({
+            drawerIcon: ({ color, size }) => getDrawerIcon('Summary', focused, color, size),
+          })}
+        />
       </Drawer.Navigator>
     </NavigationContainer>
   );
